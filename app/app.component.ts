67bf421c0f066de4de5965a6b6a58037d70b3c9b
naoki kodama <naoki.koda@gmail.com>
2016-06-29 19:23:52 +0900

    tutorial step2 end

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2><div><label>id: </label>{{hero.id}}</div><div><label>name: </label>{{hero.name}}</div>'
-
+  template:`
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div>
+    <label>name: </label>
+    <input [(ngModel)]="hero.name" placeholder="name">
+  </div>
+  `
 })
 
 export class AppComponent { 
@@ -11,10 +24,5 @@ export class AppComponent {
     id: 1,
     name: 'Windstorm'
   };
-}
-
-export class Hero {
-  id: number;
-  name: string;
 }
 
