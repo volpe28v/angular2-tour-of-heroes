9b8789b590314ea3502c51df0876180c9d2b1659
naoki kodama <naoki.koda@gmail.com>
2016-07-13 20:29:48 +0900

    step5 の途中。Promise の前

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,22 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
-
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
-
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -87,16 +73,28 @@ const HEROES: Hero[] = [
     border-radius: 4px 0 0 4px;
   }
 　`],
-  directives: [HeroDetailComponent]
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
 })
 export class AppComponent { 
-  public heroes = HEROES;
+  constructor(private heroService: HeroService) { }
+
+  heroes: Hero[];
   title = 'Tour of Heroes';
   selectedHero: Hero;
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+  
+  getHeroes() {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+  
   onSelect(hero: Hero) { this.selectedHero = hero; }
 }
 
