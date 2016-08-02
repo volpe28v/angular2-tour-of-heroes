0e08829d3c2897b80e12d90f4f32435b237e7bbb
naoki kodama <naoki.koda@gmail.com>
2016-08-02 19:52:46 +0900

    router でエラーになったが package.json と systemjs.config.js を修正したら直った

--- app/app.component.js
+++ app/app.component.js
@@ -9,8 +9,8 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var router_1 = require('@angular/router');
 var hero_service_1 = require('./hero.service');
-var heroes_component_1 = require('./heroes.component');
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
@@ -18,8 +18,8 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <h1>{{title}}</h1>\n    <my-heroes></my-heroes>\n  ",
-            directives: [heroes_component_1.HeroesComponent],
+            template: "\n  <h1>{{title}}</h1>\n    <a [routerLink]=\"['/heroes']\">Heroes</a>\n    <router-outlet></router-outlet>\n  ",
+            directives: [router_1.ROUTER_DIRECTIVES],
             providers: [
                 hero_service_1.HeroService
             ]
